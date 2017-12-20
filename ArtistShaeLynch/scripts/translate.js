c2106c3efe0c8181758441687d545785319fa65b
NixRaven <shaeolynch@gmail.com>
2017-12-19 18:22:48 -0800

    fix performance issues for artist site

--- ArtistShaeLynch/scripts/translate.js
+++ ArtistShaeLynch/scripts/translate.js
@@ -1,77 +1,81 @@
-function translateTo(fromItemIds,toItemIds) {
-  fromItemIds.forEach(function(elementFrom) {
-    document.getElementById(elementFrom).style.display = 'none';
-  });
-  toItemIds.forEach(function(elementTo) {
-    document.getElementById(elementTo).style.display = 'inline-block';
-  });
+function translateTo(itemIds,toContent,lang) {
+  for (i = 0; i < itemIds.length; i++) {
+    document.getElementById(itemIds[i]).innerHTML = toContent[i];
+    document.getElementById(itemIds[i]).lang = lang;
+  }
 }
 
-function translateToContent(fromItemIds,toItemIds) {
-  fromItemIds.forEach(function(elementFrom) {
-    document.getElementById(elementFrom).style.display = 'none';
-  });
-  toItemIds.forEach(function(elementTo) {
-    document.getElementById(elementTo).style.display = 'block';
-  });
+function translateToContent(itemIds,toContent,lang) {
+  for (i = 0; i < itemIds.length; i++) {
+    document.getElementById(itemIds[i]).innerHTML = toContent[i];
+    document.getElementById(itemIds[i]).lang = lang;
+  }
 }
 
-function setLightboxImg(imgClicked, lightboxImg, lightboxId) {
-  document.getElementById(lightboxImg).src = document.getElementById(imgClicked).src;
+function setLightboxImg(imgElem, lightboxImg, lightboxId) {
+  document.getElementById(lightboxImg).src = document.getElementById(imgElem).src;
   document.getElementById(lightboxId).style.display = 'block';
 }
     
 function initListeners() {
-  if (document.getElementById(englishText[0]).addEventListener) {
-    
-    // initiate french/english button event listener (use addEventListener)
-    document.getElementById('btnToFr').addEventListener('click', toFrench);
-    document.getElementById('btnToEn').addEventListener('click', toEnglish);
+  if (document.getElementById(textElmnts[0]).addEventListener) {
     
     if (gallery) {
       // initiate lightbox event listener (use addEventListener
       document.getElementById(lightboxElem).addEventListener('click', function() {document.getElementById(lightboxElem).style.display = 'none';});
     
       // initiate gallery image event listeners (use addEventListener
-      imgList.forEach(function(imgId) {
-        document.getElementById(imgId).addEventListener('click', (function() { setLightboxImg(imgId,lightboxImg,lightboxElem);}));
+      imgList.forEach(function(imgItem) {
+        document.getElementById(imgItem).addEventListener('click', function() { setLightboxImg(imgItem,lightboxImg,lightboxElem);});
       });
     }
   } else if (document.getElementById(englishText[0]).attachEvent) {
-    
-    // initiate french/english button event (use attachEvent)
-    document.getElementById('btnToFr').attachEvent('onclick', toFrench);
-    document.getElementById('btnToEn').attachEvent('onclick', toEnglish);
     
     if (gallery) {
       // attach event to lightbox (use attachEvent)
       document.getElementById(lightboxElem).attachEvent('onclick', function() {document.getElementById(lightboxElem).style.display = 'none';});
     
       // attach events to gallery images (use attachEvent
-      imgList.forEach(function(imgId) {
-        document.getElementById(imgId).attachEvent('onclick', (function() { setLightboxImg(imgId,lightboxImg,lightboxElem);}));
-      });
+      for (i = 0; i < imgList.length; i++) {
+        document.getElementById(imgList[i]).attachEvent('onclick', (function() { setLightboxImg(imgLinks[i],lightboxImg,lightboxElem);}));
+      }
     }
   }
 }
     
 function toEnglish() {
-  translateTo(frenchText, englishText);
+  translateTo(textElmnts, englishText,'en');
+  
+  if (document.getElementById('btnTranslate').addEventListener) {
+    document.getElementById('btnTranslate').removeEventListener('click', toEnglish);
+    document.getElementById('btnTranslate').addEventListener('click', toFrench);
+  } else if (document.getElementById('btnTranslate').attachEvent) {
+    document.getElementById('btnTranslate').attachEvent('onclick', toFrench);
+  }
+  
   if (typeof(Storage) !== 'undefined') {
     localStorage.setItem('languageShaelynch.com','en');
   }
   if (content) {
-    translateToContent(frenchContent, englishContent);
+    translateToContent(contentElmnts, englishContent,'en');
   }
 }
     
 function toFrench() {
-  translateTo(englishText, frenchText);
+  translateTo(textElmnts, frenchText,'fr');
+  
+  if (document.getElementById('btnTranslate').addEventListener) {
+    document.getElementById('btnTranslate').removeEventListener('click', toFrench);
+    document.getElementById('btnTranslate').addEventListener('click', toEnglish);
+  } else if (document.getElementById('btnTranslate').attachEvent) {
+    document.getElementById('btnTranslate').attachEvent('onclick', toEnglish);
+  }
+  
   if (typeof(Storage) !== 'undefined') {
     localStorage.setItem('languageShaelynch.com','fr');
   }
   if (content) {
-    translateToContent(englishContent, frenchContent);
+    translateToContent(contentElmnts, frenchContent,'fr');
   }
 }
 
